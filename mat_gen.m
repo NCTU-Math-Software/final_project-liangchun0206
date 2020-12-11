492bd612bdfb84f9942f6ee87317cfc8ea7a63ab
liangchun0206 <71300564+liangchun0206@users.noreply.github.com>
2020-12-11 14:54:57 +0800

    Add files via upload

---
+++ mat_gen.m
@@ -0,0 +1,46 @@
+function M=mat_gen(n)
+tic
+    M=zeros(n,n);
+    
+    M((n+1)/2,(n+1)/2)=1;
+    [row,col] = find(M==1);
+    for ii = 2:2:(n-1)
+        M(row,col+1)=((ii-1)^2)+1;
+        col=col+1;
+        val=((ii-1)^2)+1;
+        for jj=1:ii-1
+            M(row+jj,col)=val+jj;
+        end
+        val=val+ii-1;
+        [row,col] = find(M==val);
+        
+        for jj=1:ii
+            M(row,col-jj)=val+jj;
+        end
+        val=val+ii;
+        [row,col] = find(M==val);
+        
+        for jj=1:ii
+            M(row-jj,col)=val+jj;
+        end
+        val=val+ii;
+        [row,col] = find(M==val);
+        
+         for jj=1:ii
+            M(row,col+jj)=val+jj;
+        end
+        val=val+ii;
+        [row,col] = find(M==val);
+        
+    end
+    sum=0;
+    for ii =1 :n
+        sum=sum+M(ii,ii);
+    end
+    for ii =1 :n
+        sum=sum+M(n-ii+1,ii);
+    end
+    sum=sum-1;
+    disp(sum);
+ toc   
+end
